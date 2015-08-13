84ff4c6803d4694038cd669054c0141f1e323a39
Pete Corey <petecorey@gmail.com>
2015-08-13 14:29:48 -0700

    Added referralSource

--- collections/profile.js
+++ collections/profile.js
@@ -406,7 +406,8 @@ CollegeProfileSchema = new SimpleSchema({
 
   "modified": {type: Date, autoValue: function() { return new Date(); }},
   "created": fields.date(),
-  "contactable": fields.bool({optional: true})
+  "contactable": fields.bool({optional: true}),
+  "referralSource": fields.referral_source({optional: true})
 });
 
 CollegeProfiles = new Mongo.Collection("collegeprofiles");
