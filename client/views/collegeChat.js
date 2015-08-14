0d9a2653cf8691d5958cd47f5a30792aac9b5a20
Noah Huppert <noahhuppert@gmail.com>
2015-08-14 14:31:17 -0400

    Fixed unread match messages methods

--- client/views/collegeChat.js
+++ client/views/collegeChat.js
@@ -1,30 +1,67 @@
-var getUnreadMessages = function() {
-  return Matches.find({
-        "userId": Meteor.userId(),
-        "messages": {
-          $elemMatch: {
-            "read": false,
-            $or: [
-              {"sender": "college"},
-              {"sender": "admithub"}
-            ]
-          }
-        }
-      });
+var getMatchesWithUnreadMessages = function(extraQueryObj) {
+  var query = {
+    "userId": Meteor.userId(),
+    "messages": {
+      $elemMatch: {
+        "read": {$in: [false, undefined]},
+        "sender": {$in: ["college", "admithub"]}
+      }
+    }
+  };
+
+  if(extraQueryObj) {
+    query = _.extend(extraQueryObj, query);
+  }
+
+  return Matches.find(query);
+};
+
+var getMatchUnreadMessagesCount = function(match) {
+  if(!match || !match.messages || match.messages.length === 0) {
+    return 0;
+  }
+
+  var count = 0;
+
+  for(var message in match.messages) {
+    count += !message.read ? 1 : 0;
+  }
+
+  return count;
+};
+
+var getMatchesUnreadMessagesCount = function() {
+  var matchesWithUnreadMessagesCursor = getMatchesWithUnreadMessages();
+
+  if(matchesWithUnreadMessagesCursor.count() === 0) {
+    return 0;
+  }
+
+  var matchesWithUnreadMessages = matchesWithUnreadMessagesCursor.fetch();
+
+  var count = 0;
+
+  for(var i = 0; i < matchesWithUnreadMessages.length; i++) {
+    var match = matchesWithUnreadMessages[i];
+    count += getMatchUnreadMessagesCount(match);
+  }
+
+  return count;
+};
+
+var currentCollegeId;
+Template.collegeChat.created = function() {
+  currentCollegeId = new ReactiveVar(-1);
 };
 
 Template.collegeChat.helpers({
   hasUnreadMessages: function() {
-    return getUnreadMessages().count() > 0;
+    return getMatchesUnreadMessagesCount() > 0;
   },
   unreadMessageCount: function() {
-    var unreadMessages = getUnreadMessages();
+    var count = getMatchesUnreadMessagesCount();
 
-    if(unreadMessages.count() > 99) {
-      return "99+";
-    }
-
-    return unreadMessages.count();
+    return count > 99 ? "99+" : count;
   },
   matches: function() {
     return  Matches.find({userId: Meteor.userId()}).fetch();
@@ -48,15 +85,40 @@ Template.collegeChat.helpers({
     return college.name;
   },
   collegeUnreads: function(collegeId) {
-    var unreadMessages = getUnreadMessages().fetch();
+    var collegeMatchCursor = getMatchesWithUnreadMessages({
+      collegeId: collegeId
+    });
 
-    for(var match in unreadMessages) {
-      if(match.collegeId === collegeId) {
-        return match.messages.length;
-      }
+    if(!collegeMatchCursor || collegeMatchCursor.count() === 0) {
+      return undefined;
     }
-    console.log("NOOOO", getUnreadMessages().fetch());
-    return false;
+
+    var collegeMatch = collegeMatchCursor.fetch()[0];
+
+    if(!collegeMatch || !collegeMatch.messages || collegeMatch.messages.length === 0) {
+      return undefined;
+    }
+
+    var count = getMatchUnreadMessagesCount(collegeMatch);
+
+    if(count === 0) {
+      return undefined;
+    }
+
+    return count > 99 ? "99+" : count;
+  },
+  currentCollegeName: function() {
+    if(!currentCollegeId || currentCollegeId.get() === -1) {
+      return "";
+    }
+
+    var college = Colleges.findOne({_id: currentCollegeId.get()});
+
+    if(!college || !college.name) {
+      return "Unknown college";
+    }
+
+    return college.name;
   }
 });
 
@@ -66,5 +128,24 @@ Template.collegeChat.events({
   },
   "click #pg-college-chat-bar-header .icon-x": function() {
     document.querySelector("#pg-college-chat-bar-area").setAttribute("data-active", "false");
+  },
+  "click .pg-college-chat-bar-item": function(event) {
+    currentCollegeId.set($(event.currentTarget).attr("data-college-id"));
+  }
+});
+
+Template.collegeChatMessages.helpers({
+  messages: function() {
+    if(!currentCollegeId || currentCollegeId === -1) {
+      return [];
+    }
+
+    var match = Matches.findOne({collegeId: currentCollegeId});
+
+    if(!match || !match.messages) {
+      return [];
+    }
+
+    return match.messages;
   }
 });
