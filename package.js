56ee8b858d08397b8b053a5c125cd3ecbc150131
Pete Corey <petecorey@gmail.com>
2015-09-30 10:19:08 -0700

    Updated to 1.2

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "admithub:admithub-common",
   summary: "Common styles, templates, and libraries for AdmitHub sites",
-  version: "0.0.6",
+  version: "0.0.7",
   git: "https://github.com/AdmitHub/admithub-common.git"
 });
 
@@ -17,10 +17,10 @@ Package.onUse(function(api) {
     'templating',
     'underscore',
 
-    'aldeed:simple-schema',
-    'aldeed:collection2',
-    'aldeed:autoform',
-    'matb33:collection-hooks'
+    'aldeed:simple-schema@1.3.3',
+    'aldeed:collection2@2.5.0',
+    'aldeed:autoform@5.5.1',
+    'matb33:collection-hooks@0.8.0'
   ]);
 
   // begin styles
@@ -89,10 +89,10 @@ Package.onUse(function(api) {
   api.addFiles('collections/posts.js');
   api.addFiles('collections/ceebCodes.js');
   api.addFiles('collections/pendingEventReports.js');
-  api.addFiles('public/icons/fonts/icons.eot');
-  api.addFiles('public/icons/fonts/icons.svg');
-  api.addFiles('public/icons/fonts/icons.ttf');
-  api.addFiles('public/icons/fonts/icons.woff');
+  api.addAssets('public/icons/fonts/icons.eot', 'client');
+  api.addAssets('public/icons/fonts/icons.svg', 'client');
+  api.addAssets('public/icons/fonts/icons.ttf', 'client');
+  api.addAssets('public/icons/fonts/icons.woff', 'client');
 
   api.export([
     'fields',
