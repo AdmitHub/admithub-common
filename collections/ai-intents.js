06713e28caf0b11b6191036123a4714144e825ce
Brandon Horst <brandonhorst@gmail.com>
2016-02-02 17:53:59 -0500

    Remove subject - it was a denormalization of topic

--- collections/ai-intents.js
+++ collections/ai-intents.js
@@ -3,7 +3,6 @@ AIIntents.attachSchema(new SimpleSchema({
   _id: {type: String, regEx: SimpleSchema.RegEx.Id, optional: true},
   topic: {type: String},
   questions: {type: [String]},
-  subject: {type: String},
   outputContexts: {type: [String]},
   inputContexts: {type: [String]}
 }));
