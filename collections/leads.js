6b0e3f22d1b12f588d57511928223c1c7716d28e
elliott-ribner <elr886@gmail.com>
2016-03-08 15:15:26 -0500

    leads changed to simple schema for autoform

--- collections/leads.js
+++ collections/leads.js
@@ -2,11 +2,13 @@ Leads = new Mongo.Collection('leads');
 
 var o = {optional: true};
 
-Leads.attachSchema({
-  "firstName": fields.string(o),
-  "lastName": fields.string(o),
+Leads.attachSchema(new SimpleSchema({
+	"firstName": fields.string({optional: true, label: "Your Name"}),
+  "lastName": fields.string(o), //no longer using last name field, full name saved to firstName
+  "title": fields.string({optional: true, label: "Your Title"}), 
   "email": fields.email({optional: false}),
+  "phone": fields.phone_number({optional: true, label: "Phone Number"}),
   "createdAt": fields.created_date(o),
-  "organization": fields.string(o)
-});
+  "organization": fields.string({optional: true, label: "Institution Name"})
+}))
 
