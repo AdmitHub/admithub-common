4061482bb1971d19a4b883f9dc338c5accfc0f05
elliott-ribner <elr886@gmail.com>
2016-03-28 13:10:47 -0400

    nested objects are now simple schema object

--- collections/georgiaStateUsers.js
+++ collections/georgiaStateUsers.js
@@ -8,87 +8,87 @@ BrandedUserSchema = new SimpleSchema({
 	schoolEmail: fields.email(o),
 	entryYear: fields.number(o),
 	entryTerm: fields.string(o),
-	name: {
+	name: {type: new SimpleSchema({
 		first: fields.string(o),
 		last: fields.string(o),
 		middleInitial: fields.string(o),
 		full: fields.string(o),
 		nickName: fields.string(o),
-	},
+	}),optional: true},
 	phone: fields.phone_number(o),
 	dob: fields.date(o),
-	location: {
+	location: {type: new SimpleSchema({
 		address1: fields.address(o),
 		address2: fields.address(o),
 		city: fields.string(o),
 		state: fields.state(o),
 		zip: fields.zip_code(_.extend(o)),
 		country: fields.string(o),
-	},
+	}), optional: true},
 	inStateStudent: fields.bool(o),
-	application: {
+	application: {type: new SimpleSchema({
 		status: fields.string(o), 
-	},
-	profile: {
+	}), optional: true},
+	profile: {type: new SimpleSchema({
 		studentType: fields.string(o),
-	},
-	finAid: {
+	}), optional: true},
+	finAid: {type: new SimpleSchema({
 		fafsaComplete: fields.bool(o),
 		finAidInterest: fields.bool(o),
 		scholarshipAwarded: fields.bool(o),
 		scholarshipAccepted: fields.bool(o),
 		missingEntryLoan: fields.bool(o),
 		missingPromissoryLoan: fields.bool(o),
-	},
-	housing: {
+	}),optional: true},
+	housing: {type: new SimpleSchema({
 		onCampus: fields.bool(o),
 		preferenceType: fields.string(o),
 		depositPaid: fields.bool(o),
 		depositDate: fields.bool(o),
-	},
-	orientation: {
+	}),optional: true},
+	orientation:{type: new SimpleSchema ({
 		needsToRsvp: fields.bool(o),
 		attended: fields.bool(o),
 		attendedDate: fields.date(o),
 		registeredDate: fields.date(o),
-	},
-	textSetting: {
+	}),optional: true},
+	textSetting:{type: new SimpleSchema ({
 		canText: fields.bool(o),
 		softStop: fields.bool(o),
 		hardStop: fields.bool(o),
-	},
-	interest: {
+	}), optional: true},
+	interest: {type: new SimpleSchema({
 		crm: fields.number(o),
 		admithub: fields.number(o),
-	},
-	intent: {
+	}), optional: true},
+	intent: {type: new SimpleSchema({
 		intendsToEnroll: fields.bool(o),
 		intentRecievedDate: fields.date(o),
-	},
-	initialSurvey: {
-		completed: fields.bool(o),
-		skipped: fields.bool(o),
-	},
-	finAidWorkflow: {
-		completed: fields.bool(o),
-		skipped: fields.bool(o),
-	},
-	orientationWorkflow: {
-		completed: fields.bool(o),
-		skipped: fields.bool(o),
-	},
-	campusIdWorkflow: {
-		completed: fields.bool(o),
-		skipped: fields.bool(o),
-	},
-	emailSetUpWorkflow: {
-		completed: fields.bool(o),
-		skipped: fields.bool(o),
-	},
-	housingWorkflow: {
-		completed: fields.bool(o),
-		skipped: fields.bool(o),
-	}
+	}), optional: true},
+	initialBrandedSurvey: {type: new SimpleSchema({
+    skip: fields.bool(o),
+    finished: fields.bool(o),
+  }), optional: true},
+  finAidWorkflow: {type: new SimpleSchema({
+    skip: fields.bool(o),
+    finished: fields.bool(o),
+  }), optional: true},
+  orientationWorkflow: {type: new SimpleSchema({
+    skip: fields.bool(o),
+    finished: fields.bool(o),
+  }), optional: true},
+  campusIdWorkflow: {type: new SimpleSchema({
+    skip: fields.bool(o),
+    finished: fields.bool(o),
+  }), optional: true},
+  emailSetUpWorkflow: {type: new SimpleSchema({
+    skip: fields.bool(o),
+    finished: fields.bool(o),
+  }), optional: true},
+  housingWorkflow: {type: new SimpleSchema({
+    skip: fields.bool(o),
+    finished: fields.bool(o),
+  }), optional: true},
 });
 
 GeorgiaStateUsers = new Mongo.Collection('georgiaStateUsers');
