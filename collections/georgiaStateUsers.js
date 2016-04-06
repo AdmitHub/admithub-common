4f9c91c31c9612c7348022676e5c6913801595a5
Brandon Horst <brandonhorst@gmail.com>
2016-04-06 14:02:46 -0400

    add county

--- collections/georgiaStateUsers.js
+++ collections/georgiaStateUsers.js
@@ -23,6 +23,7 @@ BrandedUserSchema = new SimpleSchema({
     address2: fields.address(o),
     address3: fields.address(o),
     city: fields.string(o),
+    county: fields.string(o),
     state: fields.state(o),
     zip: fields.zip_code(_.extend(o)),
     country: fields.string(o),
