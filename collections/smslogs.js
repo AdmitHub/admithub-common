7336e05ed9d72ec2a46fa68e62a3077846f6af3e
Pete Corey <petecorey@gmail.com>
2016-04-26 17:28:43 -0400

    Added facebook as transport option in SMSLogs

--- collections/smslogs.js
+++ collections/smslogs.js
@@ -18,7 +18,7 @@ SmsLogs.attachSchema(new SimpleSchema({
   workflow: {type: String, optional: true},
   messagingService: {type: String, optional: true},
   error: {type: Boolean, defaultValue: false},
-  transport: {type: String, allowedValues: ["web", "twilio"], optional: false}
+  transport: {type: String, allowedValues: ["web", "twilio", "facebook"], optional: false}
 }));
 if (Meteor.isServer) {
   SmsLogs.allow({
