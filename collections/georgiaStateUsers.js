6a8e78a168cc331c6a081aa498cbd88814b51483
elliott-ribner <elr886@gmail.com>
2016-05-26 18:09:40 -0400

    new meta field

--- collections/georgiaStateUsers.js
+++ collections/georgiaStateUsers.js
@@ -168,6 +168,10 @@ BrandedUserSchema = new SimpleSchema({
       skip: fields.bool(o),
       finished: fields.bool(o),
     }), optional: true},
+    finAidFollowUpBot: {type: new SimpleSchema({
+      skip: fields.bool(o),
+      finished: fields.bool(o),
+    }), optional: true},
   }), optional: true},
   abGroup: {type: Number, optional: true, max: 1, min: 0, decimal: true}
 });
