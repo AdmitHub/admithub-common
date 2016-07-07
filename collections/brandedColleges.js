cb010dfab3f15daedb66f98e3f2175559fef6de1
Pete Corey <petecorey@gmail.com>
2016-07-07 16:34:49 -0400

    Added optional emailPrefix

--- collections/brandedColleges.js
+++ collections/brandedColleges.js
@@ -3,6 +3,7 @@ BrandedColleges.attachSchema(new SimpleSchema({
   _id: {type: String, regEx: SimpleSchema.RegEx.Id, optional: true},
   name: {type: String},
   messagingService: {type: String},
+  emailPrefix: {type: String, optional: true},
   oliName: {type: String},
   aiSubject: {type: String},
   collegeId: {type: String, regEx: SimpleSchema.RegEx.Id, optional: true},
