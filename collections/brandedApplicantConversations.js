f1670ae214086f6f1b2fa6e2ea8a4fd48cda5fa7
elliott-ribner <elr886@gmail.com>
2016-07-13 15:34:47 -0400

    one field added in brandedappconvo for metabase querying

--- collections/brandedApplicantConversations.js
+++ collections/brandedApplicantConversations.js
@@ -3,6 +3,7 @@ BrandedApplicantConversations.attachSchema(new SimpleSchema({
   _id: {type: String, regEx: SimpleSchema.RegEx.Id, optional: true},
   applicantId: {type: String}, // this could very well be a Mongo-style ID
   brandedCollegeId: {type: String, regEx: SimpleSchema.RegEx.Id, optional: true},
+  created: {type: Date, optional: true}, // additional createdAt date for ease of query in metabase
   messages: {type: [Object]},
   "messages.$.created": {type: Date},
   "messages.$.sender": {type: String, allowedValues: ["student", "college", "admithub"]},
