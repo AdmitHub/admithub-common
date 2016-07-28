843c8a569a95f5ea7236077c7a5d0691626526b8
Pete Corey <petecorey@gmail.com>
2016-07-28 15:09:10 -0400

    messagingService is optional

--- collections/workflows.js
+++ collections/workflows.js
@@ -1,12 +1,12 @@
 Workflows = new Mongo.Collection("workflows");
 
 Workflows.attachSchema(new SimpleSchema({
-    "_id": {type: String, regEx: SimpleSchema.RegEx.Id, optional: true},
-    "name": {type: String},
-    "messagingService": {type: String},
-    "steps": {type: [
-        new SimpleSchema({
-            "prompt": {type: String}
-        })
-    ]}
+  "_id": {type: String, regEx: SimpleSchema.RegEx.Id, optional: true},
+  "name": {type: String},
+  "messagingService": {type: String, optional: true},
+  "steps": {type: [
+    new SimpleSchema({
+      "prompt": {type: String}
+    })
+  ]}
 }));
