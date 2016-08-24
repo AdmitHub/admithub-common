2e95a5dc436b7117cd41ccfe77dfb1121a081431
Pete Corey <petecorey@gmail.com>
2016-08-24 15:30:30 -0400

    Updated workflows schema

--- collections/workflows.js
+++ collections/workflows.js
@@ -3,12 +3,12 @@ Workflows = new Mongo.Collection("workflows");
 Workflows.attachSchema(new SimpleSchema({
   "_id": {type: String, regEx: SimpleSchema.RegEx.Id, optional: true},
   "name": {type: String},
+  "humanName": {type: String, optional: true},
+  "description": {type: String, optional: true},
   "messagingService": {type: String, optional: true},
   "steps": {type: [
     new SimpleSchema({
       "name": {type: String, optional: true},
-      "humanName": {type: String, optional: true},
-      "description": {type: String, optional: true},
       "prompt": {type: String, optional: true},
       "editable": {type: Boolean},
     })
