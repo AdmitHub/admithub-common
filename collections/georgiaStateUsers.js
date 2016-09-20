ac2a6c7ef67a5233f4e09f8841e165588d7c2687
elliott-ribner <elr886@gmail.com>
2016-09-20 18:03:13 -0400

    meta field for fafsa nudge bot

--- collections/georgiaStateUsers.js
+++ collections/georgiaStateUsers.js
@@ -41,7 +41,11 @@ BrandedUserSchema = new SimpleSchema({
     studentCategory: fields.student_category(o),
     citizenVerified: fields.bool(o),
     immunizationHold: fields.bool(o),
-    emergencyContactHold: fields.bool(o)
+    emergencyContactHold: fields.bool(o),
+    hsName: fields.string(o),
+    hsZip: fields.zip_code(o),
+    hsGradYear: fields.expected_graduation_year(o),
+    intendedMajor: fields.long_string(o),
   }), optional: true},
   finAid: {type: new SimpleSchema({
     fafsaReceived: fields.bool(o), 
@@ -75,6 +79,26 @@ BrandedUserSchema = new SimpleSchema({
     wrongNumber: fields.bool(o),
     newPhone: fields.phone_number(o)
   }), optional: true},
+  tests: {type: new SimpleSchema ({
+    gpa: fields.number({decimal: true, optional: true}),
+    maxGpa: fields.number({decimal: true, optional: true}),
+    satComposite: fields.sat_composite_score(o),
+    satMath: fields.sat_score(o),
+    satReading: fields.sat_score(o), 
+    satEssay: fields.sat_essay_score(o), 
+    satAnalysis: fields.sat_essay_score(o), 
+    satWriting: fields.sat_essay_score(o), 
+    actComposite: fields.act_composite_score(o),
+    actMath: fields.act_composite_score(o),
+    actScience: fields.act_composite_score(o),
+    actEnglish: fields.act_composite_score(o),
+    actReading: fields.act_composite_score(o),
+    actWriting: fields.act_composite_score(o),
+    actIdeasAnalysis: fields.act_composite_score(o),
+    actDevAndSupport: fields.act_composite_score(o),
+    actOrganization: fields.act_composite_score(o),
+    actLangaugeConvention: fields.act_composite_score(o),
+  }),optional: true},
   interest: {type: new SimpleSchema({
     crm: fields.number({min: 0, max: 5, optional: true}),
     admithub: fields.number({min: 0, max: 5, optional: true}),
@@ -187,6 +211,38 @@ BrandedUserSchema = new SimpleSchema({
     orientationAttendedDate: fields.date(o), // orientation.attendedDate /
     orientationNeedsToRsvp: fields.bool(o), // orientation.needsToRsvp /
     intendsToEnroll: fields.bool(o) // intent.intendsToEnroll /
+  }), optional: true},
+  _gather: {type: new SimpleSchema({
+    firstName: fields.string(o), //
+    lastName: fields.string(o), //
+    middleInitial: fields.string(o), //
+    email: fields.email(o), //
+    phone: fields.phone_number(o), //
+    dob: fields.date(o), //
+    hsName: fields.string(o),
+    hsZip: fields.zip_code(o),
+    hsGradYear: fields.expected_graduation_year(o),
+    intendedMajor: fields.long_string(o),
+    zip: fields.zip_code(o), //
+    address: fields.string(o), //
+    gpa: fields.number({decimal: true, optional: true}),
+    maxGpa: fields.number({decimal: true, optional: true}),
+    satComposite: fields.sat_composite_score(o),
+    satMath: fields.sat_score(o),
+    satReading: fields.sat_score(o), 
+    satEssay: fields.sat_essay_score(o), 
+    satAnalysis: fields.sat_essay_score(o), 
+    satWriting: fields.sat_essay_score(o), 
+    actComposite: fields.act_composite_score(o),
+    actMath: fields.act_composite_score(o),
+    actScience: fields.act_composite_score(o),
+    actEnglish: fields.act_composite_score(o),
+    actReading: fields.act_composite_score(o),
+    actWriting: fields.act_composite_score(o),
+    actIdeasAnalysis: fields.act_composite_score(o),
+    actDevAndSupport: fields.act_composite_score(o),
+    actOrganization: fields.act_composite_score(o),
+    actLangaugeConvention: fields.act_composite_score(o),
   }), optional: true},
   meta: {type: new SimpleSchema({
     pounceIntroBot: {type: new SimpleSchema({
@@ -374,7 +430,18 @@ BrandedUserSchema = new SimpleSchema({
       started: fields.bool(o),
       finished: fields.bool(o),
     }), optional: true},
+    gatherProfileInfoBot: {type: new SimpleSchema({
+      skip: fields.bool(o),
+      started: fields.bool(o),
+      finished: fields.bool(o),
+    }), optional: true},
+    fafsaNudgeBot: {type: new SimpleSchema({
+      skip: fields.bool(o),
+      started: fields.bool(o),
+      finished: fields.bool(o),
+    }), optional: true},
   }), optional: true},
+  
   abGroup: {type: Number, optional: true, max: 1, min: 0, decimal: true}
 });
 
