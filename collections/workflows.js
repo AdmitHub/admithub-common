59c58f9d8cdba6500d4e187f880c245454d29d3e
Pete Corey <petecorey@gmail.com>
2016-10-31 16:00:01 -0400

    Made name optional

--- collections/workflows.js
+++ collections/workflows.js
@@ -2,7 +2,7 @@ Workflows = new Mongo.Collection("workflows");
 
 Workflows.attachSchema(new SimpleSchema({
   "_id": {type: String, regEx: SimpleSchema.RegEx.Id, optional: true},
-  "name": {type: String},
+  "name": {type: String, optional: true},
   "humanName": {type: String, optional: true},
   "description": {type: String, optional: true},
   "messagingService": {type: String, optional: true},
