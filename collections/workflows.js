5c62669d6a92029ce21c4c6598acecd280c0e2a5
Pete Corey <petecorey@gmail.com>
2016-10-31 15:00:01 -0400

    Made hidden optional

--- collections/workflows.js
+++ collections/workflows.js
@@ -6,7 +6,7 @@ Workflows.attachSchema(new SimpleSchema({
   "humanName": {type: String, optional: true},
   "description": {type: String, optional: true},
   "messagingService": {type: String, optional: true},
-  "hidden": {type: Boolean, defaultValue: false},
+  "hidden": {type: Boolean, defaultValue: false, optional: true},
   "custom": {type: Boolean, optional: true},
   "steps": {
     type: Object,
