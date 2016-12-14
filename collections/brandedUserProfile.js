0a6daa48ec5df8d70336756af74f2fdf66eb7e81
elliott-ribner <elr886@gmail.com>
2016-12-14 15:00:47 -0500

    added field for known admit opt out bot

--- collections/brandedUserProfile.js
+++ collections/brandedUserProfile.js
@@ -541,6 +541,11 @@ BrandedUserSchema = new SimpleSchema({
       started: fields.bool(o),
       finished: fields.bool(o),
     }), optional: true},
+    knownAdmitOptOutBot: {type: new SimpleSchema({
+      skip: fields.bool(o),
+      started: fields.bool(o),
+      finished: fields.bool(o),
+    }), optional: true},
   }), optional: true},
   
   abGroup: {type: Number, optional: true, max: 1, min: 0, decimal: true},
