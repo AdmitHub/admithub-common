3dda8b0b8c6c8ec019dd6ab6fb558b08879ff9fe
Damien Rochford <djrochford@gmail.com>
2017-01-23 10:44:10 -0500

    Alphabetize fields in brandedColleges schema

--- collections/brandedColleges.js
+++ collections/brandedColleges.js
@@ -1,31 +1,31 @@
 BrandedColleges = new Mongo.Collection("brandedcolleges");
 BrandedColleges.attachSchema(new SimpleSchema({
   _id: {type: String, regEx: SimpleSchema.RegEx.Id, optional: true},
-  name: {type: String},
-  messagingService: {type: String},
-  messagingServiceSid: {type: String, optional: true},
   abbr: {type: String, optional: true}, // GSU
-  tuitionDueDate: {type: Date, optional: true}, //8 -15
-  intentDeadline: {type: Date, optional: true}, // june 1
-  collection: {type: String, optional: true},
-  emailPrefix: {type: String, optional: true},
-  oliName: {type: String},
   aiSubject: {type: String},
-  collegeId: {type: String, regEx: SimpleSchema.RegEx.Id, optional: true},
-  facebookId: {type: Number, optional: true},
   animal: {type: String, optional: true}, // Panther
-  linkAdmit: {type: String, optional: true},
-  linkIntentToEnroll: {type: String, optional: true},
+  collection: {type: String, optional: true},
+  collegeId: {type: String, regEx: SimpleSchema.RegEx.Id, optional: true},
+  emailPrefix: {type: String, optional: true},
+  facebookId: {type: Number, optional: true},
+  filterLink: {type: String, optional: true},
+  filterOn: {type: Boolean, optional: true},
+  hashtag: {type: String, optional: true} //include the hash
+  intentDeadline: {type: Date, optional: true}, // june 1
+  introKnownProspectBotPhoto: {type: String, optional: true, defaultValue: ""},
   linkAcceptAid: {type: String, optional: true},
-  linkFinAid: {type: String, optional: true},
+  linkAdmit: {type: String, optional: true},
   linkFafsa: {type: String, optional: true},
-  linkOrientationInfo: {type: String, optional: true},
+  linkFinAid: {type: String, optional: true},
+  linkIntentToEnroll: {type: String, optional: true},
   linkOrientationChecklist: {type: String, optional: true},
+  linkOrientationInfo: {type: String, optional: true},
   linkTourSignUp: {type: String, optional: true},
   linkVisitTips: {type: String, optional: true},
+  messagingService: {type: String},
+  messagingServiceSid: {type: String, optional: true},
+  name: {type: String},
+  oliName: {type: String},
   phoneFinAid: {type: String, optional: true},
-  filterOn: {type: Boolean, optional: true},
-  filterLink: {type: String, optional: true},
-  introKnownProspectBotPhoto: {type: String, optional: true, defaultValue: ""},
-  hashtag: {type: String, optional: true} //include the hash
+  tuitionDueDate: {type: Date, optional: true}, //8 -15
 }));
