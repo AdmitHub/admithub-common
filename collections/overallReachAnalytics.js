0bd621a0dd4f5f2e77b311a2ae28e10d591c8abc
Rob Wilkinson <wilkinson.robert.a@gmail.com>
2017-07-01 16:10:31 -0700

    adds totalusers field

--- collections/overallReachAnalytics.js
+++ collections/overallReachAnalytics.js
@@ -1,8 +1,11 @@
-OverallReachAnalytics = new Mongo.Collection("overallReachAnalytics");
-OverallReachAnalytics.attachSchema(new SimpleSchema({
-  _id: {type: String, regEx: SimpleSchema.RegEx.Id, optional: true},
-  messagingService: { type: String, optional: true },
-  usersReached: { type: Number },
-  date: { type: Date },
-  updatedAt: {type: Date}
-}))
+OverallReachAnalytics = new Mongo.Collection('overallReachAnalytics');
+OverallReachAnalytics.attachSchema(
+  new SimpleSchema({
+    _id: { type: String, regEx: SimpleSchema.RegEx.Id, optional: true },
+    messagingService: { type: String, optional: true },
+    usersReached: { type: Number },
+    totalUsers: { type: Number },
+    date: { type: Date },
+    updatedAt: { type: Date }
+  })
+);
