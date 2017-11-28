fa41ee5e9bdb697b8280725dcc688e8092a99a0d
Anthony Reid <anthony@admithub.com>
2017-11-28 18:46:21 -0500

    Wrap neolith API

--- package.js
+++ package.js
@@ -74,6 +74,7 @@ Package.onUse(function (api) {
   api.addFiles('server/indexes.js', 'server');
   api.addFiles('server/methods.js', 'server');
   api.addFiles('server/oli.js', 'server');
+  api.addFiles('server/neolith.js', 'server');
 
 
   api.export([
@@ -100,6 +101,7 @@ Package.onUse(function (api) {
     'HsLeads',
     'Matches',
     'Oli',
+    'Neolith',
     'PendingEventReports',
     'TelescopePosts',
     'quoteRe',
