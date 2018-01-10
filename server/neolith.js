a879b951a1dd16fc9293e0fc3b8a1654961a385e
Damien Rochford <djrochford@gmail.com>
2018-01-10 11:41:29 -0500

    Update neolith server file

--- server/neolith.js
+++ server/neolith.js
@@ -1,3 +1,4 @@
+const o = {optional: true}
 /**
  * Neolith API
  */
@@ -65,15 +66,15 @@ Neolith = {
 
   // Parameter schema for ``Neolith.initiate``
   scheduleMessageParams: new SimpleSchema({
-    dialogId: {type: String, regEx: SimpleSchema.RegEx.Id},
-    query: {type: String},
-    hidden: {type: Boolean, optional: true}
-    importReportId: {type: String, regEx: SimpleSchema.RegEx.Id, optional: true},
-    recipientLabel: {type: String, optional: true},
-    selectedCalendar: {type: String, optional: true},
-    test: {type: Boolean, optional: true},
-    time: {type: String, optional: true},
-    workflowHumanName: {type: String, optional: true}
+    dialogId: fields.id(),
+    query: fields.string(),
+    hidden: fields.bool(o)
+    importReportId: fields.id(o),
+    recipientLabel: fields.string(o)
+    selectedCalendar: fields.string(o)
+    test: fields.bool(o)
+    time: fields.string(o)
+    workflowHumanName: fields.string(o)
   }),
   /**
    * Schedule a scheduledMessage
@@ -91,7 +92,7 @@ Neolith = {
   },
 
   deleteScheduledMessageParams: new SimpleSchema({
-    scheduledMessageId: {type: String}
+    scheduledMessageId: fields.id()
   }),
   /**
    * Cancel a scheduledMessage
@@ -114,11 +115,12 @@ Neolith = {
    * @param {Object} params - Parameters as defined in ``Neolith.scheduleMessage``
    */
   sendSingleMessageParams: new SimpleSchema({
-    userId: {type: String},
-    source: {type: String},
-    senderId: {type: String},
-    body: {type: String}
+    body: fields.long_string()
+    senderId: fields.id()
+    source: fields.string(),
+    userId: fields.id(),
   }),
+
   sendSingleMessage: function(params) {
     check(params, Neolith.sendSingleMessageParams);
 
@@ -129,147 +131,14 @@ Neolith = {
     );
   },
 
-  // Parameter schema for ``Neolith.initiate``
-  initiateParams: new SimpleSchema({
-    userId: {type: String, regEx: SimpleSchema.RegEx.Id},
-    // FIXME: remove transport once neolith doesn't epend on it.
-    senderId: {type: String, regEx: SimpleSchema.RegEx.Id, optional: true},
-    transport: {type: String, allowedValues: ["twilio", "web"], optional: true},
-    body: {type: String, optional: true},
-    media: {type: String, regEx: SimpleSchema.RegEx.Url, optional: true},
-    workflow: {type: String, optional: true},
-    workflowOptions: {type: Object, optional: true, blackbox: true},
-    forceRevalidate: {type: Boolean, optional: true},
-    ignorePausedUntil: {type: Boolean, optional: true},
-    validationWindow: {type: Number, optional: true},
-    prefix: {type: String, optional: true},
-    messagingService: {type: String, optional: true}
-  }),
-  /**
-   * Tell Neolith to start workflow processing for the given userId in its current
-   * state.
-   * @param {Object} params - Parameters as defined in ``Neolith.initiateParmas``
-   * schema.
-   */
-  initiate: function(params) {
-    check(params, Neolith.initiateParams);
-    params.workflowOptions = params.workflowOptions && JSON.stringify(
-      params.workflowOptions
-    );
-    if (!params.workflowOptions) {
-      delete params.workflowOptions;
-    }
-    return Neolith._callEndpoint(
-      'POST',
-      dotGet(Meteor, 'settings.neolith.initiate'),
-      params
-    );
-  },
-
-  // parameter schema for ``Neolith.handleWebMessage``
-  handleWebMessageParams: new SimpleSchema({
-    userId: {type: String, regEx: SimpleSchema.RegEx.Id},
-    body: {type: String, optional: true},
-    media: {type: String, regEx: SimpleSchema.RegEx.Url, optional: true},
-    workflow: {type: String, optional: true},
-    workflowOptions: {type: Object, optional: true}
-  }),
-  /**
-   * Forward a message posted via the web on to Neolith.
-   * @param {Object} params - Parameters as defined in
-   * ``Neolith.handleWebMessageParams`` schema
-   */
-  handleWebMessage: function(params) {
-    check(params, Neolith.handleWebMessageParams);
-    params.workflowOptions = params.workflowOptions && JSON.stringify(
-      params.workflowOptions
-    );
-    if (!params.workflowOptions) {
-      delete params.workflowOptions;
-    }
-    return Neolith._callEndpoint(
-      'POST',
-      dotGet(Meteor, 'settings.neolith.handleWebMessage'),
-      params
-    );
-  },
-
-  // parameter schema for ``Neolith.coldSMS``
-  coldSmsParams: new SimpleSchema({
-    userId: {type: String, regEx: SimpleSchema.RegEx.Id},
-    text: {type: String, optional: true},
-    media: {type: String, regEx: SimpleSchema.RegEx.Url, optional: true},
-    workflow: {type: String, optional: true},
-    workflowOptions: {type: Object, optional: true, blackbox: true},
-    forceRevalidate: {type: Boolean, optional: true},
-    validationWindow: {type: Number, optional: true},
-    messagingService: {type: String, optional: true}
-  }),
-  /**
-   * Fire off an SMS to Neolith, bypassing any usual workflow processing.
-   * @param {Object} params - Parameters as defined in ``coldSmsParams`` schema
-   */
-  coldSMS: function(params) {
-    check(params, Neolith.coldSmsParams);
-    params.workflowOptions = params.workflowOptions && JSON.stringify(
-      params.workflowOptions
-    );
-    if (!params.workflowOptions) {
-      delete params.workflowOptions;
-    }
-    return Neolith._callEndpoint('POST',
-      dotGet(Meteor, 'settings.neolith.coldSMS'),
-      params
-    );
-  },
-
-  // parameter schema for ``Neolith.officerChat``
-  officerChatParams: new SimpleSchema({
-    collegeToken: {type: String},
-    from: {type: String},
-    message: {type: String}
-  }),
-  /**
-   * Send a message from a college officer to a student
-   * @param {Object} params - Parameters as defined in ``officerChatParams`` schema
-   */
-  officerChat: function(params) {
-    check(params, Neolith.officerChatParams);
-    return Neolith._callEndpoint('POST',
-      dotGet(Meteor, 'settings.neolith.officerChat'),
-      params
-    );
-  },
-
-  // parameter schema for ``Neolith.appendMatchMessage``
-  appendMatchMessageParams: new SimpleSchema({
-    userId: {type: String, regEx: SimpleSchema.RegEx.Id},
-    collegeId: {type: String, regEx: SimpleSchema.RegEx.Id, optional: true},
-    highschoneolithd: {type: String, regEx: SimpleSchema.RegEx.Id, optional: true},
-    message: {type: String},
-    sender: {type: String, allowedValues: ["student", "highschool", "college", "admithub"]},
-    onlyIfEmpty: {type: Boolean, optional: true}
-  }),
-  /**
-   * Send a message from a student to a officer/highschool
-   * @param {Object} params - Parameters as defined in ``appendMatchMessageParams`` schema
-   */
-  appendMatchMessage: function(params) {
-    check(params, Neolith.appendMatchMessageParams);
-    return Neolith._callEndpoint('POST',
-      dotGet(Meteor, 'settings.neolith.appendMatchMessage'),
-      params
-    );
-  },
-
   // parameter schema for ``Neolith.forwardToCollege``
   forwardToCollegeParams: new SimpleSchema({
-    messagingService: {type: String},
-    userId: {type: String},
-    logId: {type: String},
-    prefix: {type: String},
-    question: {type: String},
-    email: {type: String}
+    messagingService: fields.string(),
+    userId: fields.id(),
+    logId: fields.id(),
+    prefix: fields.string(),
+    question: fields.string(),
+    email: fields.email()
   }),
   /**
    * Forwards a student message to a college
