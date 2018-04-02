f0cf275f9444670a4074d09ce5bab12d88c30856
Damien Rochford <djrochford@gmail.com>
2018-04-02 17:42:01 -0400

    Adding some fields to the smslog

--- collections/smslogs.js
+++ collections/smslogs.js
@@ -2,12 +2,15 @@ SmsLogs = new Mongo.Collection('smslogs')
 SmsLogs.attachSchema(new SimpleSchema({
   _id: {type: String, regEx: SimpleSchema.RegEx.Id, optional: true},
   createdAt: {type: Date},
-  aiLog: {type: String, optional: true},
+  events: {type: [Object]},
+  'events.$.type': {type: String},
+  'events.$.errorMessage': {type: String, optional: true}
   incoming: {type: Boolean, defaultValue: false},
+  transport: {type: String, allowedValues: ["web", "twilio", "facebook", "email"], optional: false},
+  aiLog: {type: String, optional: true},
   accountSid: {type: String, optional: true},
   body: {type: String, optional: true},
   dialogId: {type: String, optional: true},
-  error: {type: Boolean, defaultValue: false},
   facebookId: {type: String, optional: true},
   from: {type: String, optional: true},
   inReplyTo: {type: String, optional: true},
@@ -24,7 +27,7 @@ SmsLogs.attachSchema(new SimpleSchema({
   source: {type: String, optional: true},
   testUser: {type: Boolean, optional: true, defaultValue: false},
   to: {type: String, optional: true},
-  transport: {type: String, allowedValues: ["web", "twilio", "facebook", "email"], optional: false},
+  twilioSid: {type: String, optional: true},
   userNumber: {type: String, optional: true},
   userId: {type: String, optional: true}
 }))
