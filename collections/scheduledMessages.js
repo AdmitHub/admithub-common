dea7f50d056e062fbbd5bbca22fe80c953951d2d
Damien Rochford <damien@admithub.com>
2018-09-13 14:15:24 -0500

    Make all user-tracking fields in `scheduledMessages` doc expect object values; add counters for each of them.

--- collections/scheduledMessages.js
+++ collections/scheduledMessages.js
@@ -9,16 +9,19 @@ ScheduledMessages.attachSchema(new SimpleSchema({
   batchSize: {type: Number, optional: true},
   completed: {type: Boolean, defaultValue: false},
   context: {type: String, optional: true},
-  deliveryFailureUsers: {type: [String], optional: true},
+  deliveryFailureUsers: {type: Object, blackbox: true, optional: true}, // To be used as a set, with user id keys
+  deliveryFailureCount: {type: Number, optional: true}, // should equal the number of keys in `deliveryFailureUsers`
   endDate: {type: Date, optional: true},
   hidden: {type: Boolean, optional: true},
   isIntro: {type: Boolean, optional: true, defaultValue: false},
   importReportId: {type: String, optional: true},
   message: {type: String, optional: true},
-  messagedUsers: {type: Object, blackbox: true, defaultValue: {}},
+  messagedUsers: {type: Object, blackbox: true, defaultValue: {}}, // To be used as a set, with user id keys
+  messagedUsersCount: {type: Number, optional: true}, // should equal the number of keys in `messagedUsers`
   note: {type: String, optional: true},
   onGoing: {type: Boolean, optional: true},
-  optOutUsers: {type: [String], optional: true},
+  optOutUsers: {type: Object, blackbox: true, optional: true}, // To be used as a set, with user id keys
+  optOutUsersCount: {type: Number, optional: true}, // should equal the number of keys in `optOutUsers`
   paused: {type: Boolean, optional: true},
   query: {type: String, optional: true},
   recipientLabel: {type: String, optional: true},
@@ -28,7 +31,6 @@ ScheduledMessages.attachSchema(new SimpleSchema({
   startDate: {type: Date, optional: true},
   started: {type: Boolean, optional: true},
   test: {type: Boolean, optional: true},
-  usersContacted: {type: Number, optional: true},
   userSearch: {type: Object, blackbox: true, optional: true},
   weekends: {type: Boolean, optional: true},
   workflow: {type: String, optional: true},
