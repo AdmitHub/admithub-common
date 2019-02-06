e5df7f66f025a05bb9da6efbbcec1212d669b384
Anthony Reid <anthony@Anthonys-MacBook-Pro.local>
2019-02-05 20:54:49 -0500

    Add missing _id to optoutevent schema

--- collections/optOutEvents.js
+++ collections/optOutEvents.js
@@ -1,5 +1,6 @@
 OptOutEvents = new Mongo.Collection('optOutEvents');
 OptOutEvents.attachSchema(new SimpleSchema({
+  _id: {type: String, optional: true},
   type: {
     type: String,
     optional: false
