f54e533cf31f5b501efce2f07c16842aee86f63b
Damien Rochford <damien@admithub.com>
2019-02-15 16:39:34 -0600

    AH-96 add holocene-context related fields

--- collections/dataWorkflows.js
+++ collections/dataWorkflows.js
@@ -6,46 +6,59 @@ Dialogs.attachSchema(new SimpleSchema({
   'humanName': {type: String, optional: true},
   'description': {type: String, optional: true},
   'messagingService': {type: String, optional: true},
+  'contexts': {type: [String], optional: true},
   'createdAt': {type: Date, optional: true},
   'updatedAt': {type: Date, optional: true},
   'hidden': {type: Boolean, optional: true},
   'sentToUsers': {type: Boolean, optional: true},
-  'initialState': {type: String , optional: false},
+  'initialState': {type: String, optional: false},
   'states': {type: [String], optional: true},
   'expirationLength': {type: Number, optional: true},
   'reminders': {type: [Object], blackbox: true, optional: true},
-  'converted': {type: Boolean, optional: true}, //a flag to show that the dialog was converted from a data workflow by script
+  'converted': {type: Boolean, optional: true}, // a flag to show that the dialog was converted from a data workflow by script
   'isIntro': {type: Boolean, optional: true, defaultValue: false},
-  'metadata': {type: new SimpleSchema({
-    'createdBy': {type: String, optional: false},
-    'createdVia': {type: String, optional: false}
-  }), optional: true}
+  'metadata': {
+    type: new SimpleSchema({
+      'createdBy': {type: String, optional: false},
+      'createdVia': {type: String, optional: false}
+    }),
+    optional: true
+  }
 }))
 
 DialogStates = new Mongo.Collection('dialogStates')
 
 DialogStates.attachSchema(new SimpleSchema({
   '_id': {type: String, optional: true},
+  'contexts': {type: [String], optional: true},
   'name': {type: String, optional: true},
   'promptType': {type: String, optional: false},
   'prompt': {type: String, optional: false, defaultValue: ''},
-  'skip': {type: new SimpleSchema({
+  'skip': {
+    type: new SimpleSchema({
       'query': {type: String}
-    }), optional: true
+    }),
+    optional: true
   },
   'media': {type: String, optional: true},
   'parentDialog': {type: String, optional: false},
   'nextStates': {type: Object, blackbox: true},
   'enterActions': {type: [Object], blackbox: true, optional: true},
   'exitActions': {type: [Object], blackbox: true, optional: true},
-  'range': {type: new SimpleSchema({
-    min: {type: Number, optional: false},
-    max: {type: Number, optional: false}
-  }), optional: true},
+  'range': {
+    type: new SimpleSchema({
+      min: {type: Number, optional: false},
+      max: {type: Number, optional: false}
+    }),
+    optional: true
+  },
   'openPromptLabel': {type: String, optional: true},
-  'multipleChoices': {type: [new SimpleSchema({
-    prompt: {type: String, optional: false}
-  })], optional: true},
+  'multipleChoices': {
+    type: [new SimpleSchema({
+      prompt: {type: String, optional: false}
+    })],
+    optional: true
+  },
   'pauseTime': {type: Number, optional: true},
   'converted': {type: String, optional: true},
   'createdAt': {type: Date, optional: true},
