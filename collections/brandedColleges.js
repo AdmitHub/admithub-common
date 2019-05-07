79029e61bd44e2b55bf20581e355833a72ec28fa
Gabe <gmp@h4xmb.org>
2019-05-07 13:25:03 -0400

    Update brandedColleges.js

--- collections/brandedColleges.js
+++ collections/brandedColleges.js
@@ -8,6 +8,7 @@ BrandedColleges.attachSchema(new SimpleSchema({
   aiSubject: {type: String},
   animal: {type: String, optional: true}, // Panther
   askInternet: {type: Boolean, optional: true}, // AH-352
+  availableRegions: {type: [String], optional: true},
   botType: {type: [String], optional: true}, // for analysis; see AH-264
   collection: {type: String, optional: true},
   collegeId: {type: String, regEx: SimpleSchema.RegEx.Id, optional: true},
