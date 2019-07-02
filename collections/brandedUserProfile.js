9f5ea881c6353201f03698a49ec9680e119fd99f
Nancy Eckenthal <nancy.eckenthal@gmail.com>
2019-07-02 10:06:16 -0400

    adds regex pattern to _phone

--- collections/brandedUserProfile.js
+++ collections/brandedUserProfile.js
@@ -359,7 +359,10 @@ BrandedUserSchema = new SimpleSchema({
     bringCar: fields.bool(o) // _internal.needsParking
   }),
   optional: true},
-  _phone: fields.string(o),
+  _phone: {
+    type: String,
+    regEx: /^\d{10}$/
+  },
   _previousPhone: fields.string(o),
   _profile: {type: new SimpleSchema({
     description: fields.profile_description(o),
